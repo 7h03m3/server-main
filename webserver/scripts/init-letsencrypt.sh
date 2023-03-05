9ac7df483c7e8f752e77fc14083845f8fa4ab0cf
Thomas Stanger <stangth1@students.zhaw.ch>
2023-03-05 15:23:16 +0100

    chore(webserver): add fsgw.ch domain to init script

--- webserver/scripts/init-letsencrypt.sh
+++ webserver/scripts/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(ohrbuehl.info www.ohrbuehl.info)
+domains=(ohrbuehl.info www.ohrbuehl.info fsgw.ch www.fsgw.ch)
 rsa_key_size=4096
 data_path="../data/certbot"
 email="thomas.stanger@staswiss.ch" 
